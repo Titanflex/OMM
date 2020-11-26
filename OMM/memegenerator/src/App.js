50ba7e9efb54f7c9c66b27ebebc6795776f91177
Tabea Blenk <tabeablenk@gmail.com>
2020-11-26 13:58:24 +0100

    minor format fix

--- OMM/memegenerator/src/App.js
+++ OMM/memegenerator/src/App.js
@@ -2,19 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        h1 > Our super cool Meme Generator is coming soon... < /h1> <
-        /header> <
-        /div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Our super cool Meme Generator is coming soon...</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
