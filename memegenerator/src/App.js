d61c1e455e3dc331eb766fe9dc56efa2b87073cb
Tabea Blenk <tabeablenk@gmail.com>
2020-11-26 13:40:45 +0100

    created basic react project

---
+++ memegenerator/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        h1 > Our super cool Meme Generator is coming soon... < /h1> <
+        /header> <
+        /div>
+    );
+}
+
+export default App;
