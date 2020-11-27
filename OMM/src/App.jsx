14529ee8e5d7e987a9d2817fbefd15ae1c37d373
Tabea Blenk <tabeablenk@gmail.com>
2020-11-27 17:39:30 +0100

    first version meme generator with no functionality

---
+++ OMM/src/App.jsx
@@ -0,0 +1,20 @@
+import "./css/App.css";
+import Container from "react-bootstrap/Container";
+import MemeContainer from "./MemeContainer";
+import FormContainer from "./FormContainer";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function App() {
+  return (
+    <Container class="AppContainer">
+      <h1>Best Meme Generator</h1>
+      <Row>
+        <Col><MemeContainer></MemeContainer></Col>
+        <Col><FormContainer></FormContainer></Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
