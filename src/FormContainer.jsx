5475d71d44bac2887fe7d0d760a4bb34d7e2b3da
Tabea Blenk <tabeablenk@gmail.com>
2020-11-28 19:27:40 +0100

    added RootContainer; added Callbacks

--- src/FormContainer.jsx
+++ src/FormContainer.jsx
@@ -1,47 +1,52 @@
-import './css/Form.css';
-import React, {Component} from 'react';
+import "./css/Form.css";
+import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import MemeContainer from "./MemeContainer";
 
-
-
 export default class FormContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            caption1: '',
-            caption2: '',
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      caption1: "",
+      caption2: "",
+    };
 
-        this.handleChange = this.handleChange.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
+  }
 
-    }
+//   handleChange = (event) => {
+//     let name = event.target.name;
+//     let value = event.target.value;
+//     this.setState({ [name]: value });
+//   };
 
-    handleChange = (event) => {
-        let name = event.target.name;
-        let value = event.target.value;
-        this.setState({[name]: value});
-    }
-
-    render() {
-        return (
-            <Container>
-                <Row>
-                    <Col><MemeContainer caption1 ={this.state.caption1} caption2={this.state.caption2}/></Col>
-                    <Col>
-                        <Form>
-                            <Form.Group>
-                                <Form.Control type="email" placeholder="Upper text" name="caption1" onChange={this.handleChange}/>
-                                <Form.Control type="email" placeholder="Lower text" name="caption2" onChange={this.handleChange}/>
-                            </Form.Group>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-
-        );
-    }
+  render() {
+    return (
+      <Container>
+        <Form>
+          <Form.Group>
+            <Form.Control
+              type="email"
+              placeholder="Upper text"
+              name="caption1"
+              onChange={(event) => {
+                this.props.onChangeCaption1(event.target.value)
+              }}
+            />
+            <Form.Control
+              type="email"
+              placeholder="Lower text"
+              name="caption2"
+              onChange={(event) => {
+                this.props.onChangeCaption2(event.target.value)
+              }}
+            />
+          </Form.Group>
+        </Form>
+      </Container>
+    );
+  }
 }
