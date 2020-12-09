cb2c789e1da6d2f8498aa6887e586d760226ca2e
Titanflex <felix.grelka@gmail.com>
2020-12-09 13:06:33 +0100

    Task4

--- OMM/src/MemeContainer.jsx
+++ OMM/src/MemeContainer.jsx
@@ -21,12 +21,16 @@ export default class MemeContainer extends Component {
         if (this.state.numberOfImages > 0) {
             this.setState({ currentMemeIndex: this.state.currentMemeIndex === 0 ? this.state.numberOfImages - 1 : this.state.currentMemeIndex - 1 })
         }
+        console.log(this.props.caption1);
+        this.props.changeHandler(this.state.memes[this.state.currentMemeIndex].upper, this.state.memes[this.state.currentMemeIndex].lower)
     };
 
     previousMeme = () => {
         if (this.state.numberOfImages > 0) {
             this.setState({ currentMemeIndex: this.state.currentMemeIndex === this.state.numberOfImages - 1 ? 0 : this.state.currentMemeIndex + 1 })
         }
+        this.setState({ caption1: this.state.memes[this.state.currentMemeIndex].upper, caption2: this.state.memes[this.state.currentMemeIndex].lower })
+        this.props.changeHandler(this.state.memes[this.state.currentMemeIndex].upper, this.state.memes[this.state.currentMemeIndex].lower)
     };
 
     getMoreMemes = async () => {
@@ -43,6 +47,31 @@ export default class MemeContainer extends Component {
         }
     }
 
+    loadMeme = async () => {
+        const res = await fetch('http://localhost:3030/memeIO/get-memes');
+        const json = await res.json();
+        console.log(json)
+        this.setState({ numberOfImages: json.memes.length, memes: json.memes, currentMemeIndex: 0 });
+    }
+
+    saveMeme = () => {
+        fetch('http://localhost:3030/memeIO/save-meme', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                url: this.state.memes[this.state.currentMemeIndex].url,
+                upper: this.props.caption1,
+                lower: this.props.caption2
+            })
+        }).then(res => {
+            this.loadMeme();
+        })
+        console.log('save');
+    }
+
     render() {
         var currentMeme = this.renderMeme();
         return (
@@ -53,6 +82,10 @@ export default class MemeContainer extends Component {
                 <div className='navigation'>
                     <NextPrevButton handleClick={this.previousMeme} variant="outlined" color="secondary">❮</NextPrevButton>
                     <NextPrevButton handleClick={this.nextMeme} variant="outlined" color="secondary">❯</NextPrevButton>
+                </div>
+                <div className='navigation'>
+                    <Button onClick={this.loadMeme} variant="outlined" color="secondary">Load</Button>
+                    <Button onClick={this.saveMeme} variant="outlined" color="secondary">Save</Button>
                 </div>
 
                 <div className="memeContainer">
